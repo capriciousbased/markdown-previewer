27b6e67441668264d5133c8602c515523b49a44b
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-05-13 09:22:11 +0200

    task id added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import ReactMarkdown from "react-markdown";
 import "./styles.css";
 
 export default function App() {
-  const [markdown, setMarkdown] = useState("Heading");
+  const [markdown, setMarkdown] = useState("Enter your Markdown");
 
   return (
-    <div className="markdown__container">
-      <textarea
-        value={markdown}
-        onChange={(e) => setMarkdown(e.target.value)}
-      />
-
-      <ReactMarkdown source={markdown} className="markdown__preview" />
+    <div>
+      <h2>Convert your Markdown</h2>
+      <div className="markdown__container">
+        <textarea
+          id="editor"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        />
+        <div id="preview">
+          <ReactMarkdown source={markdown} className="markdown__preview" />
+        </div>
+      </div>
     </div>
   );
 }
